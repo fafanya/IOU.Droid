0d16c8c8f3e4407407965db8ad77b2c96a93b57d
eugene afanasovich <eugeneafanasovich@gmail.com>
2016-09-20 20:01:05 +0300

    последние изменения

--- share/share/Adapters/ViewPagerAdapter.cs
+++ share/share/Adapters/ViewPagerAdapter.cs
@@ -1,16 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.Content;
-using Android.OS;
-
 using Android.Support.V4.App;
-using Android.Support.V7.App;
-using Android.Support.V7.Widget;
-using Android.Support.Design.Widget;
-using Android.Support.V4.View;
 
 namespace share
 {
